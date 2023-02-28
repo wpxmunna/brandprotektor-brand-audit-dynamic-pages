4a7d74bd59a108ec6d7cc076b275213757f92924
wpxmunna <wpxmunna@gmail.com>
2023-03-01 00:04:50 +0600

    React and tailwind installation and setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import './index.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="bg-hero-bg h-[350px]">
+          <img src="./Assets/Group.png" className="object-center w-[160px] h-[40px] align-middle"/>
+      </section>
     </div>
   );
 }
